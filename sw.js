719b45a1856c4426865128d95f7f4dec041122c1
beniskossi <68175656+beniskossi@users.noreply.github.com>
2025-04-04 00:13:59 +0000

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,8 @@
+// sw.js
+self.addEventListener('install', (event) => {
+    console.log('Service Worker installé');
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(fetch(event.request));
+});
